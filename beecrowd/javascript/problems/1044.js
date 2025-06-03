b83f415f00de4234ca8e57e50768c04625a81e24
http.dluc <http.dluc@gmail.com>
2025-06-02 21:01:53 -0300

    att

---
+++ beecrowd/javascript/problems/1044.js
@@ -0,0 +1,8 @@
+export function problem (lines){
+    const [a, b] = lines[0].split(" ").map(Number)
+    if(a % b == 0 || b % a == 0){
+        console.log("Sao Multiplos")
+    }else{
+        console.log("Nao sao Multiplos")
+    }
+}
---
+++ beecrowd/javascript/problems/1045.js
@@ -0,0 +1,35 @@
+export const problem = (lines) => {
+    let [a, b, c] = lines[0].split(" ").map(Number)
+    
+    const ordenarLados = (lados) => lados.sort((x, y) => y - x)
+    const verificarTriangulo = ([a, b, c]) => a < b + c
+
+    const triangulos = ([a, b, c]) => {
+        const mensagem = []
+        if(a >= b + c){
+            mensagem.push("NAO FORMA TRIANGULO")
+        }
+        if(a*a == b*b + c*c){
+            mensagem.push("TRIANGULO RETANGULO")
+        }
+        if(a*a > b*b + c*c){
+            mensagem.push("TRIANGULO OBTUSANGULO")
+        }
+        if(a*a < b*b + c*c){
+            mensagem.push("TRIANGULO ACUTANGULO")
+        }else if(a==b && b == c){
+            mensagem.push("TRIANGULO EQUILATERO")
+        }
+        return mensagem.join("\n")
+    }
+    const principal = (a, b, c)=> {
+        const ladosOrdenados = ordenarLados([a, b, c])
+        if(verificarTriangulo(ladosOrdenados)){
+            const classificacao = triangulos(ladosOrdenados)
+            console.log(classificacao)
+        }else{
+            console.log("NAO FORMA TRIANGULO")
+        }
+    }
+    principal(a,b,c)
+} 
